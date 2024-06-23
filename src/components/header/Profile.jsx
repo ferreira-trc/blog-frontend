1b3846a3f03233cac0e9382f451504004bf4cb95
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-24 00:46:55 +0100

    feat: creation of the profile button

---
+++ src/components/header/Profile.jsx
@@ -0,0 +1,12 @@
+
+export function Profile({onClick}) {
+
+    
+    return (
+        <>
+            <div className="profile">                             
+                <button className='bottonProfile' onClick={onClick}></button>                                
+            </div>            
+        </>
+    );
+}
