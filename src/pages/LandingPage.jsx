e5208c31278ccf5bea6630a719ff78ad7070aed2
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-23 21:14:08 +0100

    feat: add the connection to the API

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,54 @@
-import {Post} from "../components/post/Post"
-import posts from '../data/posts.json';
+import React, { useState, useEffect } from 'react';
+import { Post } from '../components/post/Post';
 
 export function LandingPage() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [order, setOrder] = useState('default'); // Estado para controle da ordenação
+
+    const fetchPosts = async (order = 'default') => {
+        try {
+            setLoading(true);
+            const url = order === 'cron' 
+                ? 'http://127.0.0.1:8080/post?order=cron' 
+                : 'http://127.0.0.1:8080/post';
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Erro ao buscar os posts');
+            }
+            const data = await response.json();
+            setPosts(data);
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts(order);
+    }, [order]); // Dependência para refazer a requisição quando a ordem mudar
+
+    const handleOrderChange = () => {
+        setOrder((prevOrder) => (prevOrder === 'default' ? 'cron' : 'default'));
+    };
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    if (error) {
+        return <div>Erro: {error}</div>;
+    }
+
     return (
         <>
+            <button onClick={handleOrderChange}>Ordenar por ordem cronológica</button>
             <main>
-                {posts.map( (p) => {
-                    return (
-                        <Post post={p} key={p.id}/>
-                    );
-                })}
+                {posts.map((p) => (
+                    <Post post={p} key={p.id} />
+                ))}
             </main>
         </>
     );
