67d7bc32178324ee71cc42db2ace5f0868ffd5cc
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-24 00:46:22 +0100

    feat: creation of the menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import { Navlink } from "./NavLink"
+import links from "../data/hamburguer.json"
+
+export function Menu({navClass, ulClass, clas}) {
+
+    
+    return (
+        <>
+            <nav className={navClass}>
+                <ul className={ulClass}>
+                    {links.map((link) => (
+                        <Navlink url={link.url} name={link.name} clas={clas}/>
+                        ))
+                    }                                       
+                </ul>
+            </nav>
+        </>
+    )
+}
