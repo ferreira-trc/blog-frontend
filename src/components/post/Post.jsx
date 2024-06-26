bc879d97b6ac12bdb72506d22c67320a3d4bf4de
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-26 21:53:05 +0100

    feat: changed the order of the title and author

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,28 +1,36 @@
-import { useState } from "react";
 import { Button } from "../Button.jsx";
 import { Icon } from "./Icon";
 
 export function Post({post}) {
+    
     return(
         <>
-              <article className="lp-article" key={post.id}>
-                        <section className="lp-section-header">                            
-                            <h2>{post.title}</h2>
-                        </section> 
+            <article className="lp-article" key={post.id}>
 
-                        <section>
-                            <p>{post.text}</p>
-                        </section>
-                        
-                        <section className="lp-section-bottom">                            
-                            <div>
-                                <Button  clase={"lp-button"}>
-                                    <span>comment</span>
-                                    <Icon name={"comment"}/>
-                                </Button>
-                           </div>
-                        </section> 
-                    </article>
+                <section className="lp-section-title">                                        
+                    <h2>{post.title}</h2>                  
+                </section> 
+                
+                <section className="lp-section-author">
+                    <h3>{post.authorUserName}</h3> 
+                </section>
+
+                <section className="lp-section-category">
+                    <h6>{post.category}</h6>
+                </section>
+
+                <section className="lp-body">
+                    <p>{post.text}</p>
+                </section>
+                
+                <section className="lp-section-bottom">                            
+                    <div>
+                        <Button  clase={"lp-button"}>                            
+                            <Icon name={"comment"}/>
+                        </Button>
+                    </div>
+                </section> 
+            </article>
         </>
     )
 }
